16c94e9a4d61c5aa5e10fec1dd878c4e8c04ccdd
Dharmavarapu Ajay Chakravarthy <ajaychakravarthy.d@gmail.com>
2020-09-28 17:47:21 +0530

    File

--- todo/todo/src/todo/Todo.java
+++ todo/todo/src/todo/Todo.java
@@ -14,79 +14,74 @@ import java.util.Scanner;
  * @author ajayc
  */
 public class Todo {
-    
-   private static final String newLine = System.getProperty("line.separator");
-                                         //return OS dependent line seprator
+
+    private static final String newLine = System.getProperty("line.separator");
+    //return OS dependent line seprator
     public static void main(String[] args) throws IOException {
         // TODO code application logic 
-        
+
         String fileName = "myObjectives.txt"; // name of the file
         Scanner sc = new Scanner(System.in);
         int choice;
-        while(true)
-        {
-            System.out.println("Please Choose an option (1) Add a task "
-                    + "(2) List all the tasks "
-                    + "(3) Update a task (4) Remove a task (0) Exit ");
-            choice =sc.nextInt();
-            switch(choice) //user input 
+        while (true) {
+            System.out.println("Please Choose an option (1) Add a task " +
+                "(2) List all the tasks " +
+                "(3) Update a task (4) Remove a task (0) Exit ");
+            choice = sc.nextInt();
+            switch (choice) //user input 
             {
                 case 1:
-                    System.out.println("Add a task (Id, name,date(dd/mm/yyyy),"
-                            + " status)");
+                    System.out.println("Add a task (Id, name,date(dd/mm/yyyy)," +
+                        " status)");
                     Scanner ip = new Scanner(System.in);
-                     TodoList todo = new TodoList();
+                    TodoList todo = new TodoList();
                     int id = ip.nextInt();
                     todo.name = ip.next();
                     todo.date = ip.next();
-                    todo.status=Status.valueOf(ip.next());
-                    String note = String.valueOf(id)+"\t"+todo.name+"\t"
-                            +todo.date+"\t"+todo.status;
+                    todo.status = Status.valueOf(ip.next());
+                    String note = String.valueOf(id) + "\t" + todo.name + "\t" +
+                        todo.date + "\t" + todo.status;
                     PrintWriter myObj = null;
-                    myObj = new PrintWriter(new FileOutputStream(fileName, 
-                            true));
-                    myObj.write(note + newLine);//writing into text file
-                    myObj.close();// closing
+                    myObj = new PrintWriter(new FileOutputStream(fileName,
+                        true));
+                    myObj.write(note + newLine); //writing into text file
+                    myObj.close(); // closing
                     break;
                 case 2:
                     System.out.print("List of all the tasks\n");
-                    BufferedReader br_list = new BufferedReader
-                                      (new FileReader(fileName));
-                    String st; 
-                    while ((st = br_list.readLine()) != null) /*reading from 
-                                                                 *text file
-                                                                  */       
-                        System.out.println(st); /* printing all the records
-                                                            *in the text file
-                                                */
+                    BufferedReader br_list = new BufferedReader(new FileReader(fileName));
+                    String st;
+                    while ((st = br_list.readLine()) != null)
+                        /*reading from 
+                         *text file
+                         */
+                        System.out.println(st);
+                    /* printing all the records
+                     *in the text file
+                     */
                     break;
                 case 3:
-                    System.out.println("Enter the ID of the task you"
-                            + " want to Update: ");
+                    System.out.println("Enter the ID of the task you" +
+                        " want to Update: ");
                     String find_id = sc.next();
                     String s;
-                    String[] words =null;
+                    String[] words = null;
                     String oldText = "";
-                    BufferedReader br_update = new BufferedReader
-                                                (new FileReader(fileName));
-                    while ((s = br_update.readLine()) != null)
-                    {
+                    BufferedReader br_update = new BufferedReader(new FileReader(fileName));
+                    while ((s = br_update.readLine()) != null) {
                         words = s.split("\t");
-                        System.out.println(words[3]);//splitting on word length
-                        if(words[0].equals(find_id))
-                        {
+                        System.out.println(words[3]); //splitting on word length
+                        if (words[0].equals(find_id)) {
                             System.out.println("Update the status");
                             sc.nextLine();
                             String new_status = sc.nextLine();
                             String replaced = s.replaceAll(words[3],
-                                    new_status);//replacing that word with new 
+                                new_status); //replacing that word with new 
                             oldText += replaced + "\n";
                             System.out.println(oldText);
                             break;
-                        }
-                        else
-                        {
-                            oldText += s +"\n";
+                        } else {
+                            oldText += s + "\n";
                         }
                     }
                     FileWriter writer = new FileWriter(fileName);
@@ -95,14 +90,13 @@ public class Todo {
                     break;
                 case 4:
                     System.out.println("Enter the id to remove the task: ");
-                    String remove_id = sc.next();//taking ID to remove 
-                    BufferedReader reader = new BufferedReader
-                                             (new FileReader(fileName));
+                    String remove_id = sc.next(); //taking ID to remove 
+                    BufferedReader reader = new BufferedReader(new FileReader(fileName));
                     String line = null;
                     String text = "";
-                    while((line=reader.readLine())!=null)//reading text in file
+                    while ((line = reader.readLine()) != null) //reading text in file
                     {
-                        if(!line.contains(remove_id))// if line has that ID 
+                        if (!line.contains(remove_id)) // if line has that ID 
                         {
                             text += line + "\n";
                         }
@@ -112,7 +106,7 @@ public class Todo {
                     remove.close();
                     break;
                 case 0:
-                    System.exit(0);//exit 
+                    System.exit(0); //exit 
             }
         }
     }
